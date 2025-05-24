22c0e7a31a48318186fd44c9c9f6fa12d5da9b3b
Ishaan1103 <ishaanrana2635@gmail.com>
2025-05-24 17:39:51 +0000

    Added some Zap in Db and test the hook

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 const app = express();
 
 const PORT = 3000;
@@ -10,10 +13,28 @@ app.get("/", (_req, res) => {
     res.json({ "message": "Hello World!" });
 });
 
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
     const userId = req.params.userId;
     const zapId = req.params.zapId;
+    const body = req.body;
+    // store in db a new trigger
+    await client.$transaction(async (tx: any) => {
+        const run = await client.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            }
+        });
+        await client.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id
+            }
+        })
+    })
 
+    //push it on to a queue (Kafka/redis)
+
+    res.json({ message: "webhook recieved" })
 });
 
 app.listen(PORT, () => {
